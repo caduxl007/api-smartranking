54c58cad8ec60654871893a2387854c6ffc190fa
caduxl007 <eduardo2020dev@gmail.com>
2021-08-05 22:40:16 -0300

    controllers and services

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,10 +1,56 @@
-import { Body, Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CategoriasService } from './categorias.service';
+import { AtualizarCategoriaDTO } from './dtos/atualizar-categoria.dto';
+import { CriarCategoriaDTO } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
 
 @Controller('api/v1/categorias')
 export class CategoriasController {
-  constructor(){}
+  constructor(private readonly categoriasService: CategoriasService) {}
 
+  @Post()
+  @UsePipes(ValidationPipe)
   async criarCategoria(
-    @Body(criarCategoriaDto: )
-  )
+    @Body() criarCategoriaDto: CriarCategoriaDTO,
+  ): Promise<Categoria> {
+    return await this.categoriasService.criarCategoria(criarCategoriaDto);
+  }
+
+  @Get()
+  async consultarCategorias(): Promise<Categoria[]> {
+    return await this.categoriasService.consultarTodasCategorias();
+  }
+
+  @Get('/:categoria')
+  async consultarCategoriaPeloId(
+    @Param('categoria') categoria: string,
+  ): Promise<Categoria> {
+    return await this.categoriasService.consultarCategoriaPeloId(categoria);
+  }
+
+  @Put('/:categoria')
+  @UsePipes(ValidationPipe)
+  async atualizarCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDTO,
+    @Param('categoria') categoria: string,
+  ): Promise<void> {
+    await this.categoriasService.atualizarCategoria(
+      categoria,
+      atualizarCategoriaDto,
+    );
+  }
+
+  @Post('/:categoria/jogadores/:idJogador')
+  async atribuirCategoriaJogador(@Param() params: string[]): Promise<void> {
+    return await this.categoriasService.atribuirCategoriaJogador(params);
+  }
 }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { AtualizarCategoriaDTO } from './dtos/atualizar-categoria.dto';
+import { CriarCategoriaDTO } from './dtos/criar-categoria.dto';
+import { Categoria } from './interfaces/categoria.interface';
 
 @Injectable()
-export class CategoriasService {}
+export class CategoriasService {
+  constructor(
+    @InjectModel('Categoria') private readonly categoriaModel: Model<Categoria>,
+  ) {}
+
+  async criarCategoria(
+    criarCategoriaDto: CriarCategoriaDTO,
+  ): Promise<Categoria> {
+    const { categoria } = criarCategoriaDto;
+
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({ categoria })
+      .exec();
+
+    if (categoriaEncontrada) {
+      throw new BadRequestException(`Categoria ${categoria} já cadastrada`);
+    }
+
+    const categoriaCriada = new this.categoriaModel(criarCategoriaDto);
+
+    return await categoriaCriada.save();
+  }
+
+  async consultarTodasCategorias(): Promise<Categoria[]> {
+    return await this.categoriaModel.find().exec();
+  }
+
+  async consultarCategoriaPeloId(categoria: string): Promise<Categoria> {
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({ categoria })
+      .exec();
+
+    if (!categoriaEncontrada) {
+      throw new NotFoundException(`Categoria ${categoria} não encontrada`);
+    }
+
+    return categoriaEncontrada;
+  }
+
+  async atualizarCategoria(
+    categoria: string,
+    atualizarCategoria: AtualizarCategoriaDTO,
+  ): Promise<void> {
+    const categoriaEncontrada = await this.categoriaModel
+      .findOne({ categoria })
+      .exec();
+
+    if (!categoriaEncontrada) {
+      throw new NotFoundException(`Categoria ${categoria} não encontrada`);
+    }
+
+    await this.categoriaModel
+      .findOneAndUpdate({ categoria }, { $set: atualizarCategoria })
+      .exec();
+  }
+}
